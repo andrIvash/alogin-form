7c8fc3a66bc521f3bafe651613da30a57ecc708d
andrivash <andr.ivas12@gmail.com>
2017-01-03 15:29:05 +0200

    remove data to ngoninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,23 +20,22 @@ export class AppComponent implements OnInit {
     public tabGroup: ElementRef;
     private tab: Observable<Event>;
     private currentTabState: string = '#signup';
-    public constructor(private _formBuilder: FormBuilder, private _validationService: ValidationService) {
-        this.signUpForm = new FormGroup({
-            fname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)])),
-            lname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)])),
-            email: new FormControl('', _validationService.emailValidation),
-            password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)]))
-        });
-        this.loginForm = this._formBuilder.group({
-            email: ['', _validationService.emailValidation],
-            password: ['', Validators.compose([Validators.required, Validators.maxLength(8), Validators.minLength(3)])]
-        });
-    }
+    public constructor(private _formBuilder: FormBuilder, private _validationService: ValidationService) {}
     public ngOnInit() {
         this.tab = Observable.fromEvent<Event>(this.tabGroup.nativeElement, 'click');
         this.tab.subscribe((event: Event) => {
             this.currentTabState = (event.target as HTMLAnchorElement).hash;
             console.log(this.currentTabState);
+        });
+        this.signUpForm = new FormGroup({
+            fname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)])),
+            lname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)])),
+            email: new FormControl('', this._validationService.emailValidation),
+            password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)]))
+        });
+        this.loginForm = this._formBuilder.group({
+            email: ['', this._validationService.emailValidation],
+            password: ['', Validators.compose([Validators.required, Validators.maxLength(8), Validators.minLength(3)])]
         });
     }
     public submit(ev: Event, value: {[key: string]: string}) {
